b6bda9da574d18b4b9aa72a3663f834ea8d55b87
Dmytrovertosenko <dmytrovertosenko@Dmytros-Mac.local>
2024-04-29 23:49:54 +0300

    fixed swagger implementation

--- src/core/decorators/open-api.decorator.ts
+++ src/core/decorators/open-api.decorator.ts
@@ -1,4 +1,4 @@
 import { applyDecorators } from '@nestjs/common';
 import { ApiBody, ApiBasicAuth } from '@nestjs/swagger';
 
-export const OpenApi = (dto: new () => any) => applyDecorators(ApiBody({ type: [dto] }), ApiBasicAuth('api-key'));
+export const OpenApi = (dto: new () => any) => applyDecorators(ApiBody({ type: dto }), ApiBasicAuth('api-key'));
